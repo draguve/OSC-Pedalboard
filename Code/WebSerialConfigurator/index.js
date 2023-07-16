e93a7347b2cdd91329d3915aca7789606f55832e
Draguve <ritwikpatil1998@gmail.com>
2023-07-16 22:30:49 +0530

    Added code to push settings to pedal

--- Code/WebSerialConfigurator/index.js
+++ Code/WebSerialConfigurator/index.js
@@ -7,11 +7,27 @@ const encoder = new TextEncoder();
 const decoder = new TextDecoder();
 this.messages = [];
 
+const onconnect = (e) => {
+  console.log(id + 'device connected', e);
+  this.port = e.target;
+  this.physicallyConnected = true;
+};
+navigator.serial.addEventListener('connect', onconnect);
+
+// notification for a USB device getting physically disconnected
+const ondisconnect = (e) => {
+  console.log(id + ' disconnect');
+  this.physicallyConnected = false;
+  this.open = false;
+}
+navigator.serial.addEventListener('disconnect', ondisconnect);
+
 function delay(time) {
-  return new Promise(resolve => setTimeout(resolve, time));
+  return new Promise((resolve) => setTimeout(resolve, time));
 }
 
 window.addEventListener("DOMContentLoaded", (event) => {
+  $("#ConnectForm").hide();
   const notSupported = document.getElementById("NotSupported");
   const connectButton = document.getElementById("ConnectForm");
   var isSerialSupported = "serial" in navigator;
@@ -22,7 +38,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
   }
 });
 
-async function sendData(pedal_port,input_data){
+async function sendData(pedal_port, input_data) {
   if (pedal_port.writable) {
     const writer = pedal_port.writable.getWriter();
     await writer.write(encoder.encode(input_data + "\r\n"));
@@ -30,25 +46,24 @@ async function sendData(pedal_port,input_data){
   }
 }
 
-async function sendDataGetOutput(pedal_port,input_data){
+async function sendDataGetOutput(pedal_port, input_data) {
   this.messages = [];
-  await sendData(pedal_port,input_data);
+  await sendData(pedal_port, input_data);
   await delay(500);
-  try{
+  try {
     return JSON.parse(this.messages.join(""));
-  }catch{
-    return {"parseFailed":true};
+  } catch {
+    return { parseFailed: true };
   }
 }
 
 async function monitor() {
-  console.log('monitor()');
+  console.log("monitor()");
   while (pedal_port && pedal_port?.readable) {
     const reader = pedal_port.readable.getReader();
     this.open = true;
     try {
       while (this.open) {
-        console.log('reading...');
         const { value, done } = await reader.read();
         if (done) {
           // |reader| has been canceled.
@@ -60,16 +75,54 @@ async function monitor() {
         this.messages.push(decoded);
       }
     } catch (error) {
-      console.error('reading error', error);
+      console.error("reading error", error);
     } finally {
       reader.releaseLock();
     }
   }
 }
 
-async function GetSettingsFromDevice(){
-  var settings = await sendDataGetOutput(pedal_port,'{"getSettings":1}');
-  console.log(settings);
+async function GetSettingsFromDevice() {
+  var settings = await sendDataGetOutput(pedal_port, '{"getSettings":1}');
+  UpdateSettingsFromDevice(settings);
+  M.toast({ html: "Updated Settings" });
+}
+
+async function UpdateSettingsFromDevice(settings) {
+  $("#Wifi_SSID").val(settings.ssid);
+  $("#password").val(settings.password);
+  $("#IPAddress").val(settings.OSC_Server);
+  $("#Port").val(settings.OSC_Port);
+  M.updateTextFields();
+}
+
+async function PushSettingsToDevice() {
+  port = parseInt($("#Port").val()) || 8000;
+  ip = $("#IPAddress").val();
+  if (!ValidateIPaddress(ip)){
+    M.toast({ html: "Could not push, Invalid IP" });
+    return;
+  }
+  data = {
+    ssid: $("#Wifi_SSID").val(),
+    OSC_Server: ip,
+    password: $("#password").val(),
+    OSC_Port: port,
+  };
+  settingsToPush = JSON.stringify(data);
+  await sendData(pedal_port, settingsToPush);
+  M.toast({ html: "Settings pushed to device" });
+}
+
+function ValidateIPaddress(ipaddress) {
+  if (
+    /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(
+      ipaddress
+    )
+  ) {
+    return true;
+  }
+  return false;
 }
 
 async function connectToPedal() {
@@ -83,8 +136,14 @@ async function connectToPedal() {
       flowControl: "none",
     });
     monitor();
-    var settings = await sendDataGetOutput(pedal_port,'{"getSettings":1}');
-    console.log(settings);
+    var settings = await sendDataGetOutput(pedal_port, '{"getSettings":1}');
+    if (!("password" in settings)) {
+      M.toast({ html: "Could not connect to any device" });
+      return;
+    }
+    UpdateSettingsFromDevice(settings);
+    $("#ConnectForm").show();
+    $("#ConnectButton").hide();
     M.toast({ html: "Connected To Pedal" });
   } catch (error) {
     console.log(error);
