c033a9993ebb5d2d222618e246dae1b687d4fa87
Draguve <ritwikpatil1998@gmail.com>
2024-02-15 10:32:54 -0800

    Setup lwip and tinyosc build

--- Code/Midi/main.c
+++ Code/Midi/main.c
@@ -1,8 +1,61 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include <string.h>
+#include <stdlib.h>
+    //#include "lwipopts.h"
+
+#include "pico/cyw43_arch.h"
+#include "lwip/pbuf.h"
+#include "lwip/udp.h"
+
+#define UDP_PORT 4444
+#define BEACON_MSG_LEN_MAX 127
+#define BEACON_TARGET "255.255.255.255"
+#define BEACON_INTERVAL_MS 1000
+
+void run_udp_beacon() {
+    struct udp_pcb* pcb = udp_new();
+
+    ip_addr_t addr;
+    ipaddr_aton(BEACON_TARGET, &addr);
+
+    int counter = 0;
+    while (true) {
+        struct pbuf *p = pbuf_alloc(PBUF_TRANSPORT, BEACON_MSG_LEN_MAX+1, PBUF_RAM);
+        char *req = (char *)p->payload;
+        memset(req, 0, BEACON_MSG_LEN_MAX+1);
+        snprintf(req, BEACON_MSG_LEN_MAX, "%d\n", counter);
+        err_t er = udp_sendto(pcb, p, &addr, UDP_PORT);
+        pbuf_free(p);
+        if (er != ERR_OK) {
+            printf("Failed to send UDP packet! error=%d", er);
+        } else {
+            printf("Sent packet %d\n", counter);
+            counter++;
+        }
+
+        sleep_ms(BEACON_INTERVAL_MS);
+    }
+}
 
 int main() {
-    setup_default_uart();
-    printf("Hello, world!\n");
+    stdio_init_all();
+
+    if (cyw43_arch_init()) {
+        printf("failed to initialise\n");
+        return 1;
+    }
+
+    cyw43_arch_enable_sta_mode();
+
+    printf("Connecting to Wi-Fi...\n");
+    if (cyw43_arch_wifi_connect_timeout_ms("Draguve4", "pioneer123", CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+        printf("failed to connect.\n");
+        return 1;
+    } else {
+        printf("Connected.\n");
+    }
+    run_udp_beacon();
+    cyw43_arch_deinit();
     return 0;
 }
