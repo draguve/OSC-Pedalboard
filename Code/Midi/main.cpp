de6aa198b4196bec56d644a804c7260120d44fe1
Draguve <ritwikpatil1998@gmail.com>
2024-03-02 01:39:53 -0800

    hopefully it keeps working

--- Code/Midi/main.cpp
+++ Code/Midi/main.cpp
@@ -41,97 +41,59 @@ void run_udp_beacon() {
     }
 }
 
-//void core1_main(){
-//    char uname[40];
-//    char wifiname[40];
-//    pico_get_unique_board_id_string(uname,10);
-//    sprintf(wifiname,"Pedal %s",uname);
-//
-//    if (cyw43_arch_init()) {
-//        printf("failed to initialise\n");
-//        while(true){
-//            printf("Could not start wifi\n");
-//            return;
-//        }
-//    }
-//
-//    cyw43_arch_enable_sta_mode();
-//
-//    printf("Connecting to Wi-Fi...\n");
-//    if (cyw43_arch_wifi_connect_timeout_ms("Draguve4", "pioneer123", CYW43_AUTH_WPA2_AES_PSK, 30000)) {
-//        printf("failed to connect. starting ap mode\n");
-//        cyw43_arch_disable_sta_mode();
-//        cyw43_arch_enable_ap_mode(wifiname,uname,CYW43_AUTH_WPA2_AES_PSK);
-//    } else {
-//        printf("Connected.\n");
-//    }
-//
-//    while(true){
-////        tud_task();
-//        sleep_ms(100);
-//    }
-//    cyw43_arch_deinit();
-//}
-
-/* This MIDI example send sequence of note (on/off) repeatedly. To test on PC, you need to install
- * synth software and midi connection management software. On
- * - Linux (Ubuntu): install qsynth, qjackctl. Then connect TinyUSB output port to FLUID Synth input port
- * - Windows: install MIDI-OX
- * - MacOS: SimpleSynth
- */
-
-//--------------------------------------------------------------------+
-// MACRO CONSTANT TYPEDEF PROTYPES
-//--------------------------------------------------------------------+
-
-/* Blink pattern
- * - 250 ms  : device not mounted
- * - 1000 ms : device mounted
- * - 2500 ms : device is suspended
- */
-enum  {
-    BLINK_NOT_MOUNTED = 250,
-    BLINK_MOUNTED = 1000,
-    BLINK_SUSPENDED = 2500,
-};
-
 void core1_main(){
     iohandler_init();
     float volts[4] = {0, 0, 0, 0};
     bool stomps[2] = {false,false};
     while (true) {
         iohandler_get_current_state(volts,stomps);
-//        printf("%d %d A:%f B:%f C:%f D:%f\n",stomps[0],stomps[1],volts[0],volts[1],volts[2],volts[3]);
+        printf("%d %d A:%f B:%f C:%f D:%f\n",stomps[0],stomps[1],volts[0],volts[1],volts[2],volts[3]);
         busy_wait_ms(10);
     }
 }
 
-static uint32_t blink_interval_ms = BLINK_NOT_MOUNTED;
-
-void led_blinking_task(void);
-void midi_task(void);
+bool repeating_timer_callback(struct repeating_timer *t) {
+    tud_task();
+    return true;
+}
 
 /*------------- MAIN -------------*/
 int main(void)
 {
     board_init();
-
-    // init device stack on configured roothub port
     tud_init(BOARD_TUD_RHPORT);
     stdio_init_all();
+    struct repeating_timer timer;
+    add_repeating_timer_ms(10, repeating_timer_callback, NULL, &timer);
+
     multicore_launch_core1(core1_main);
-    iohandler_init();
-    float volts[4] = {0, 0, 0, 0};
-    bool stomps[2] = {false,false};
-    cyw43_arch_init();
-    while (1)
-    {
-        tud_task(); // tinyusb device task
-        led_blinking_task();
-        midi_task();
+
+    char uname[40];
+    char wifiname[40];
+    pico_get_unique_board_id_string(uname,10);
+    sprintf(wifiname,"Pedal %s",uname);
+
+    if (cyw43_arch_init()) {
+        printf("failed to initialise\n");
+        while(true){
+            printf("Could not start wifi\n");
+            sleep_ms(1000);
+        }
     }
-
-
+    cyw43_arch_enable_sta_mode();
+    printf("Connecting to Wi-Fi...\n");
+    if (cyw43_arch_wifi_connect_timeout_ms("Draguve4", "pioneer123", CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+        printf("failed to connect. starting ap mode\n");
+        cyw43_arch_disable_sta_mode();
+        cyw43_arch_enable_ap_mode(wifiname,uname,CYW43_AUTH_WPA2_AES_PSK);
+    } else {
+        printf("Connected.\n");
+    }
+    while(true){
+//        tud_task();
+        sleep_ms(100);
+    }
+    cyw43_arch_deinit();
     return 0;
 }
 
@@ -140,31 +102,20 @@ int main(void)
 //--------------------------------------------------------------------+
 
 // Invoked when device is mounted
-void tud_mount_cb(void)
-{
-    blink_interval_ms = BLINK_MOUNTED;
-}
+void tud_mount_cb(void){}
 
 // Invoked when device is unmounted
-void tud_umount_cb(void)
-{
-    blink_interval_ms = BLINK_NOT_MOUNTED;
-}
+void tud_umount_cb(void){}
 
 // Invoked when usb bus is suspended
 // remote_wakeup_en : if host allow us  to perform remote wakeup
 // Within 7ms, device must draw an average of current less than 2.5 mA from bus
-void tud_suspend_cb(bool remote_wakeup_en)
-{
+void tud_suspend_cb(bool remote_wakeup_en) {
     (void) remote_wakeup_en;
-    blink_interval_ms = BLINK_SUSPENDED;
 }
 
 // Invoked when usb bus is resumed
-void tud_resume_cb(void)
-{
-    blink_interval_ms = BLINK_MOUNTED;
-}
+void tud_resume_cb(void){}
 
 // Invoked when cdc when line state changed e.g connected/disconnected
 void tud_cdc_line_state_cb(uint8_t itf, bool dtr, bool rts)
@@ -175,74 +126,4 @@ void tud_cdc_line_state_cb(uint8_t itf, bool dtr, bool rts)
     if (dtr) {
 //        tud_cdc_write_str("Connected\n");
     }
-}
-
-//--------------------------------------------------------------------+
-// MIDI Task
-//--------------------------------------------------------------------+
-
-// Variable that holds the current position in the sequence.
-uint32_t note_pos = 0;
-
-// Store example melody as an array of note values
-uint8_t note_sequence[] =
-        {
-                74,78,81,86,90,93,98,102,57,61,66,69,73,78,81,85,88,92,97,100,97,92,88,85,81,78,
-                74,69,66,62,57,62,66,69,74,78,81,86,90,93,97,102,97,93,90,85,81,78,73,68,64,61,
-                56,61,64,68,74,78,81,86,90,93,98,102
-        };
-
-void midi_task(void)
-{
-    static uint32_t start_ms = 0;
-
-    uint8_t const cable_num = 0; // MIDI jack associated with USB endpoint
-    uint8_t const channel   = 0; // 0 for channel 1
-
-    // The MIDI interface always creates input and output port/jack descriptors
-    // regardless of these being used or not. Therefore incoming traffic should be read
-    // (possibly just discarded) to avoid the sender blocking in IO
-    uint8_t packet[4];
-    while ( tud_midi_available() ) tud_midi_packet_read(packet);
-
-    // send note periodically
-    if (board_millis() - start_ms < 286) return; // not enough time
-    start_ms += 286;
-
-    // Previous positions in the note sequence.
-    int previous = (int) (note_pos - 1);
-
-    // If we currently are at position 0, set the
-    // previous position to the last note in the sequence.
-    if (previous < 0) previous = sizeof(note_sequence) - 1;
-
-    // Send Note On for current position at full velocity (127) on channel 1.
-    uint8_t note_on[3] = { 0x90 | channel, note_sequence[note_pos], 127 };
-    tud_midi_stream_write(cable_num, note_on, 3);
-
-    // Send Note Off for previous note.
-    uint8_t note_off[3] = { 0x80 | channel, note_sequence[previous], 0};
-    tud_midi_stream_write(cable_num, note_off, 3);
-
-    // Increment position
-    note_pos++;
-
-    // If we are at the end of the sequence, start over.
-    if (note_pos >= sizeof(note_sequence)) note_pos = 0;
-}
-
-//--------------------------------------------------------------------+
-// BLINKING TASK
-//--------------------------------------------------------------------+
-void led_blinking_task(void)
-{
-    static uint32_t start_ms = 0;
-    static bool led_state = false;
-
-    // Blink every interval ms
-    if ( board_millis() - start_ms < blink_interval_ms) return; // not enough time
-    start_ms += blink_interval_ms;
-
-    board_led_write(led_state);
-    led_state = 1 - led_state; // toggle
 }
