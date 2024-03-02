21d378f5f2a0384905dee65c12d3c7701f272f6d
Draguve <ritwikpatil1998@gmail.com>
2024-03-02 02:20:26 -0800

    Added dhcp and dnsserver

--- Code/Midi/main.cpp
+++ Code/Midi/main.cpp
@@ -10,11 +10,17 @@
 #include "pico/multicore.h"
 #include "bsp/board.h"
 
+#include "lwip/tcp.h"
+extern "C"{
+    #include "dhcpserver/dhcpserver.h"
+    #include "dnsserver/dnsserver.h"
+}
+
+
 #define UDP_PORT 4444
 #define BEACON_MSG_LEN_MAX 127
 #define BEACON_TARGET "255.255.255.255"
 #define BEACON_INTERVAL_MS 1000
-
 
 void run_udp_beacon() {
     struct udp_pcb *pcb = udp_new();
@@ -47,7 +53,7 @@ void core1_main(){
     bool stomps[2] = {false,false};
     while (true) {
         iohandler_get_current_state(volts,stomps);
-        printf("%d %d A:%f B:%f C:%f D:%f\n",stomps[0],stomps[1],volts[0],volts[1],volts[2],volts[3]);
+//        printf("%d %d A:%f B:%f C:%f D:%f\n",stomps[0],stomps[1],volts[0],volts[1],volts[2],volts[3]);
         busy_wait_ms(10);
     }
 }
@@ -60,6 +66,14 @@ bool repeating_timer_callback(struct repeating_timer *t) {
 /*------------- MAIN -------------*/
 int main(void)
 {
+    if (cyw43_arch_init()) {
+        printf("failed to initialise\n");
+        while(true){
+            printf("Could not start wifi\n");
+            sleep_ms(1000);
+        }
+    }
+
     board_init();
     tud_init(BOARD_TUD_RHPORT);
     stdio_init_all();
@@ -72,25 +86,31 @@ int main(void)
     char wifiname[40];
     pico_get_unique_board_id_string(uname,10);
     sprintf(wifiname,"Pedal %s",uname);
-
-    if (cyw43_arch_init()) {
-        printf("failed to initialise\n");
-        while(true){
-            printf("Could not start wifi\n");
-            sleep_ms(1000);
-        }
-    }
     cyw43_arch_enable_sta_mode();
     printf("Connecting to Wi-Fi...\n");
     if (cyw43_arch_wifi_connect_timeout_ms("Draguve4", "pioneer123", CYW43_AUTH_WPA2_AES_PSK, 30000)) {
         printf("failed to connect. starting ap mode\n");
         cyw43_arch_disable_sta_mode();
         cyw43_arch_enable_ap_mode(wifiname,uname,CYW43_AUTH_WPA2_AES_PSK);
+
+        ip_addr_t gw;
+        ip4_addr_t mask;
+        IP4_ADDR(ip_2_ip4(&gw), 192, 168, 4, 1);
+        IP4_ADDR(ip_2_ip4(&mask), 255, 255, 255, 0);
+
+        // Start the dhcp server
+        dhcp_server_t dhcp_server;
+        dhcp_server_init(&dhcp_server, &gw, &mask);
+
+        // Start the dns server
+        dns_server_t dns_server;
+        dns_server_init(&dns_server, &gw);
+
     } else {
         printf("Connected.\n");
     }
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
     while(true){
-//        tud_task();
         sleep_ms(100);
     }
     cyw43_arch_deinit();
@@ -102,10 +122,14 @@ int main(void)
 //--------------------------------------------------------------------+
 
 // Invoked when device is mounted
-void tud_mount_cb(void){}
+void tud_mount_cb(void){
+//    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+}
 
 // Invoked when device is unmounted
-void tud_umount_cb(void){}
+void tud_umount_cb(void){
+//    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+}
 
 // Invoked when usb bus is suspended
 // remote_wakeup_en : if host allow us  to perform remote wakeup
